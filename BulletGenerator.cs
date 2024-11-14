fbb375643c78f6b170ffcfb17ba69198a2211683
Júllya Lopes Machado <123588130+jullyalopesmachado@users.noreply.github.com>
2024-11-14 09:32:53 -0500

    Update BulletGenerator.cs

--- BulletGenerator.cs
+++ BulletGenerator.cs
@@ -28,13 +28,7 @@ public class BulletGenerator : MonoBehaviour
 
     }
 
-    //In the game, we will create a Bullet Generator in front of the gun and apply this script to it.
-    //Then, we will create a bullet model, make it to a prefab object, and assign it as the "bullet" game object referenced in this script. 
-
-    //When firing, it will generate this bullet prefab, and make it to move in the Z axis (the front direction of the Bullet Generator).
-    //The move speed is 3000 (declared in the begining of this script, and you can change the number to change the script).
-    //After 1.5 seconds (the "1.5f" below is the time), it will destroy this bullet.
-    //(You can extend the time if you would like it to fly further.)
+   
 
     IEnumerator Fire()
     {
